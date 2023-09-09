b6e1af9923b942f36756758f67968a3c59c2abfb
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-09 09:58:15 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    # Constructor (initializer) method
+    def __init__(self, name):
+        self.name = name
 
-    #Instance method definition
-    pass
+    # Instance method to bark
+    def bark(self):
+        print(f"{self.name} says Woof!")
+
+    # Instance method to sit
+    def sit(self):
+        print(f"{self.name} is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+    # Constructor (initializer) method
+    def __init__(self, name):
+        self.name = name
 
-    #Instance method definition
-    pass
+    # Instance method to talk
+    def talk(self):
+        print(f"{self.name} says Hello World!")
+
+    # Instance method to walk
+    def walk(self):
+        print(f"{self.name} is walking.")
